2b3be6c7ba4ba3ea0f4dcc05e0c8825acd79ac74
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-01 10:33:11 +0800

    override

--- tutorials/ComplexExample.scala
+++ tutorials/ComplexExample.scala
@@ -1,11 +1,15 @@
 class Complex(real: Double, imaginary: Double) {
     def re = real
     def im = imaginary
+
+    override def toString() = 
+      "" + re + (if (im < 0) "" else "+") + im + "i"
 }
 
 object ComplexNumbers {
     def main(args: Array[String]): Unit = {
         val c = new Complex(1.2, 3.4)
         println("imaginary part: " + c.im)
+        println("complex number: " + c)
     }
 }
