87eacea43da2dbc40743c89915c32501e5ab3158
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-01 10:17:10 +0800

    TimerAnonymous.scala

---
+++ tutorials/TimerAnonymous.scala
@@ -0,0 +1,14 @@
+object TimerAnonymous {
+    def oncePerSecond(callback: () => Unit): Unit = {
+        while(true) {
+            callback()
+            Thread sleep 1000
+        }
+    }
+
+    def main(args: Array[String]): Unit = {
+        oncePerSecond(() => 
+            println("time flies like an arrow ...")
+        )
+    }
+}
