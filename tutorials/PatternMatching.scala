53b2bda3fbe9b667e49e68d0fc5bbc7cfb19c69b
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-01 16:05:27 +0800

    lab

--- tutorials/PatternMatching.scala
+++ tutorials/PatternMatching.scala
@@ -1,5 +1,6 @@
 abstract class Tree
 case class Sum(l: Tree, r: Tree) extends Tree
+case class Div(l: Tree, r: Tree) extends Tree
 case class Var(n: String) extends Tree
 case class Const(v: Int) extends Tree
 
@@ -8,12 +9,13 @@ object PatternMatchingLab {
 
     def eval(t: Tree, env: Environment): Int = t match {
         case Sum(l, r) => eval(l, env) + eval(r, env)
+        case Div(l, r) => eval(l, env) - eval(r, env)
         case Var(n) => env(n)
         case Const(v) => v
     }
 
     def main(args: Array[String]): Unit = {
-        val exp: Tree = Sum(Sum(Var("x"), Var("x")), Sum(Const(7), Var("y")))
+        val exp: Tree = Div(Sum(Var("x"), Var("x")), Sum(Const(7), Var("y")))
         val env: Environment = { case "x" => 5 case "y" => 7 }
         println("Expression: " + exp)
         println("Evaluation with x=5, y=7: " + eval(exp, env))
