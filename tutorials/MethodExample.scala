a7a2fbb9e45562fc9fc4ba81762f98e00819cbca
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-06 09:27:23 +0800

    match

--- tutorials/MethodExample.scala
+++ tutorials/MethodExample.scala
@@ -1,6 +1,15 @@
 
 object Math {
     def add(x: Int, y: Int): Int = x+y
+
+    def convertBooleanToString(bool: Boolean): String = {
+        if (bool) "true" else "false"
+    }
+
+     def convertBooleanToStringMessage(bool: Boolean): String = bool match {
+         case true => "you said true"
+         case false => "you said false"
+     }
 }
 
 object MethodLab {
@@ -9,5 +18,7 @@ object MethodLab {
     def main(args: Array[String]): Unit = {
         println("addThenMultiply(1, 2)(3)=" + addThenMultiply(1, 2)(3))
         println("add(1, 2)=" + Math.add(1, 2))
+        println("true=" + Math.convertBooleanToString(true))
+        println("true message=" + Math.convertBooleanToStringMessage(true))
     }
 }
