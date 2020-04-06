a679bf979e5e96c0c6938ba3be6b009e99e324f1
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-06 09:17:11 +0800

    function lab

--- tutorials/MethodExample.scala
+++ tutorials/MethodExample.scala
@@ -1,8 +1,13 @@
 
+object Math {
+    def add(x: Int, y: Int): Int = x+y
+}
+
 object MethodLab {
     def addThenMultiply(x: Int, y: Int)(multiplier: Int): Int = (x + y) * multiplier
 
     def main(args: Array[String]): Unit = {
         println("addThenMultiply(1, 2)(3)=" + addThenMultiply(1, 2)(3))
+        println("add(1, 2)=" + Math.add(1, 2))
     }
 }
